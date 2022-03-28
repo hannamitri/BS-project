6982ed237306422ec29838fd8c94cabbbfea7ff7
barry <nicolai.bernhard03@gmail.com>
2022-03-28 21:37:52 +0200

    Cleaning

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,5 @@
+import styles from "Nav.module.scss";
+
+export const Nav = () => {
+  return <div>Nav</div>;
+};
---
+++ src/components/Test.js
@@ -0,0 +1,15 @@
+// useEffect(() => {
+//  async function test() {
+//    const data = {
+//      Name: "Jeff",
+//      Email: "jeffbezos@gmail.com",
+//      Password: "coolkiddo",
+//      Location: "afghanistan",
+//      isProfessional: 1,
+//      pn: "123456789",
+//    };
+//    const countries = await insertUser(data);
+//    console.log(countries);
+//  }
+//  test();
+//   // }, []);
