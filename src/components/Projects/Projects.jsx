51ed3c1ed4fa0cdc1a940b7d329586a86ee6162a
Nathaliesaab <84041880+Nathaliesaab@users.noreply.github.com>
2022-04-05 20:18:58 +0300

    Fixed databse queries worked on project_page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,33 +1,20 @@
 import React, { useEffect, useState } from "react";
-import { getAllProjects, getUsersbyProject } from "../../api/api";
+import { getAllProjects } from "../../api/api";
 import Project from "../UI/Project/Project";
 import projects from "./Projects.module.scss";
 
 const Projects = () => {
+
+
   const [allProjects, setAllProjects] = useState([]);
-  const [users, setUsers] = useState([]);
 
   const getProjects = async () => {
     const data = await getAllProjects();
     setAllProjects(data);
   };
 
-  const getUsersByProjectID = async () => {
-    let project = {
-      project_id: 1,
-    };
-
-    await getUsersbyProject(project)
-      .then((th) => console.log(th))
-      .catch((err) => console.log(err));
-  };
-
   useEffect(() => {
     getProjects();
-    console.log(
-      `USER BY PROJECT ID:"
-        ${getUsersByProjectID()}`
-    );
   }, []);
 
   return (
@@ -39,6 +26,7 @@ const Projects = () => {
             name={project.name}
             category={project.category}
             date_created={project.date_created}
+            id={project.project_id}
           />
         ))}
       </div>
