fbd024e541bfd9b833f5c35f9c3b5d9e53cb07d5
Nathaliesaab <84041880+Nathaliesaab@users.noreply.github.com>
2022-04-06 20:16:19 +0300

    changes

---
+++ src/pages/ProjectForm/ProjectForm.jsx
@@ -0,0 +1,23 @@
+import styles from "./ProjectForm.module.scss";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+
+export const ProjectForm = () => {
+
+
+    useEffect(() => {
+
+
+    }, []);
+
+
+
+
+    return (
+        <div className={styles.container}>
+            <h1>Hi</h1>
+        </div>
+    );
+}
+
