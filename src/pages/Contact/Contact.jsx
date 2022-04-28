5d752b8b18a464a7310d7c523043f1d9477b869a
Nathaliesaab <84041880+Nathaliesaab@users.noreply.github.com>
2022-04-28 22:32:01 +0300

    contact us page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -3,42 +3,37 @@ import { FaUserAlt } from "react-icons/fa";
 import { TextInput, Box, Textarea, Button } from "@mantine/core";
 import { HiOutlineAtSymbol } from "react-icons/hi";
 import ContactImage from "../../images/Contact/contact.webp";
-import Sidebar from "../../components/Sidebar/Sidebar";
 
 export const Contact = () => {
   return (
     <div className="flex">
       <div className="contact__container">
-        <h1 className="contact__title">GET IN TOUCH!</h1>
         <div className="contact__wrapper">
           <div>
-            <img src={ContactImage} alt="" width={400} height={400} />
-          </div>
-
-          <div>
-            <Box sx={{ minWidth: 400 }} mx="auto" className="contact__content">
+            <Box sx={{ minWidth: 350 }} mx="auto" className="contact__content">
+              <h1 className="contact__title">GET IN TOUCH!</h1>
               <form>
                 <TextInput
                   required
                   icon={<FaUserAlt size={16} />}
-                  label="Name"
                   placeholder="Your name"
                 />
                 <TextInput
                   required
                   icon={<HiOutlineAtSymbol size={16} />}
-                  label="Email"
                   placeholder="Your@email.com"
                 />
                 <Textarea
                   required
-                  label="Message"
                   placeholder="Your Message..."
                   minRows={6}
                 />
                 <button className="contact__button button">Contact now</button>
               </form>
             </Box>
+          </div>
+          <div>
+            <img src={ContactImage} alt="" width={390} height={370} />
           </div>
         </div>
       </div>
