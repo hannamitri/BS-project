6a051507bb96932f5bd576d8dc7f02b7ca1c888e
hanna.mitri <hanna.mitri@hicmedia.com>
2022-05-01 18:29:53 +0300

    fixed contact and account

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Account.scss";
 import deafultAvatar from "../../images/default-avatar.png";
-import { MdLocationOn } from 'react-icons/md'
+import { MdLocationOn } from "react-icons/md";
 import { MdSettingsPhone } from "react-icons/md";
-import { HiOutlineAtSymbol } from "react-icons/hi"
+import { HiOutlineAtSymbol } from "react-icons/hi";
 import { FaUserAlt } from "react-icons/fa";
 const Account = ({ users }) => {
   const currentUser = users.data?.find(
@@ -13,41 +13,34 @@ const Account = ({ users }) => {
 
   return (
     <div className="account__container">
-      <div className="account__wrapper" >
-
-        <div className="account">
-          <div className="account__profile--avatar">
-            {currentUser?.profile ? (
-              <img src={currentUser?.profile} alt="" />
-            ) : (
-              <img src={deafultAvatar} alt="" />
-            )}
-          </div>
-          <div className="user__details">
-            <div className="user__name">
-              <FaUserAlt />
-              {currentUser?.user_name}
-            </div>
-            <div className="user__location">
-              <MdLocationOn />
-              {currentUser?.Location}
-            </div>
-            <div>
-              <HiOutlineAtSymbol  />
-              {currentUser?.email}
-            </div>
-            <div>
-              <MdSettingsPhone  />
-              {currentUser?.phone_number}
-            </div>
-          </div>
-
+      <div className="account__wrapper">
+        <div className="account__profile--avatar">
+          {currentUser?.profile ? (
+            <img src={currentUser?.profile} alt="" />
+          ) : (
+            <img src={deafultAvatar} alt="" />
+          )}
         </div>
-
-
-        <div className="account__profile"></div>
+        <div className="account__user--details">
+          <div>
+            <FaUserAlt />
+            {currentUser?.user_name}
+          </div>
+          <div>
+            <MdLocationOn />
+            {currentUser?.Location}
+          </div>
+          <div>
+            <HiOutlineAtSymbol />
+            {currentUser?.email}
+          </div>
+          <div>
+            <MdSettingsPhone />
+            {currentUser?.phone_number}
+          </div>
+        </div>
       </div>
-    </div >
+    </div>
   );
 };
 
